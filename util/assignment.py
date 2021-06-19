a1ab8746ae821126fddaaf939213b75119f53322
Parker Hutchinson <parker.hutch@outlook.com>
2021-06-19 14:23:39 -0600

    course name variable & better printing

--- util/assignment.py
+++ util/assignment.py
@@ -1,16 +1,19 @@
-class Assignment:  # TODO courseName variable?
-    def __init__(self, name, submitted):
+class Assignment:
+    # TODO should add course name here
+    def __init__(self, name, course_name, submitted):
         self.name = name
+        self.course_name = course_name
         self.submitted = submitted
         self.url = ""
-        self.release_date = None  # TODO store this also
+        self.release_date = None
         self.due_date = None
         self.late_due_date = None
 
     def __init__(
-        self, name, url, submitted, release_date, due_date, late_due_date
+        self, name, course_name, url, submitted, release_date, due_date, late_due_date
     ):
         self.name = name
+        self.course_name = course_name
         self.url = url
         self.submitted = submitted
         self.release_date = release_date
@@ -18,5 +21,6 @@ class Assignment:  # TODO courseName variable?
         self.late_due_date = late_due_date
 
     def __str__(self):
-        return f"{self.name} Due Date: {self.due_date} Submission Status: {self.submitted}"
+        submission_emoji = '\U00002705' if self.submitted else '\U0000274C'
+        return f"\U0001F4D3[{self.course_name}] {self.name} Due \U0001F4C5{self.due_date:%m/%d/%y %I:%M%p} Submitted: {submission_emoji}"
 
